3001c125972b4a7a94fe9de80adaa88fafd7ada1
Olaf Lüke <olaf@tinkerforge.com>
2016-09-15 11:04:27 +0200

    Add stub for communication

---
+++ software/src/communication.c
@@ -0,0 +1,32 @@
+/* gps-v2-bricklet
+ * Copyright (C) 2016 Olaf Lüke <olaf@tinkerforge.com>
+ *
+ * communication.c: TFP protocol message handling
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#include "communication.h"
+
+#include "bricklib2/protocols/tfp/tfp.h"
+
+BootloaderHandleMessageReturn handle_message(const void *message, void *return_message) {
+	switch(tfp_get_fid_from_message(message)) {
+//		case FID_XXX: return xxx(message, return_message);
+//		case FID_YYY: return yyy(message, return_message);
+		default: return HANDLE_MESSAGE_RETURN_NOT_SUPPORTED;
+	}
+}
